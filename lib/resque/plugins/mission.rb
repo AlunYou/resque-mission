745388440434ca3e5121c1fe51d0e5a76c7f7e7a
Alun <xianlun.you@autodesk.com>
2015-01-18 00:03:07 +0800

    add retry label

--- lib/resque/plugins/mission.rb
+++ lib/resque/plugins/mission.rb
@@ -146,8 +146,15 @@ module Resque
         # This is needed to be used with resque scheduler
         # http://github.com/bvandenbos/resque-scheduler
         def self.scheduled(queue, klass, *args)
-          Rails.logger.info ""
-          Rails.logger.info "This is a retry job: #{klass}, #{args}"
+          job_status_id, job_args = args
+          job_args = job_args['args']
+          mission_id = job_args['mission_id']
+          mission_key = "mission:#{mission_id}"
+          Rails.cache.increment(mission_key, 1)
+
+          already_retry_num = Rails.cache.increment(mission_key, 0)
+          Rails.logger.info "\nThis is #{already_retry_num}th retry job: mission_key=#{mission_key}, #{klass}, #{args.to_s}"
+
           Resque.enqueue_to(queue, klass, *args)
           uuid = args[0]
         end
